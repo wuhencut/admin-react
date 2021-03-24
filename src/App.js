405adfe70927d5e99301297fd1ad4f2f6b13c6b1
stone <stone_rivers@163.com>
2021-03-24 22:32:24 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Index from "./pages/Index/Index";
+import Login from "./pages/Login/Login";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Switch>
+        <Route path="/login" component={Login}></Route>
+        <Route path="/" component={Index}></Route>
+      </Switch>
+    );
+  }
 }
-
 export default App;
---
+++ src/pages/Index/Index.js
@@ -0,0 +1,8 @@
+import { Divider } from "antd"
+import React from "react"
+class Index extends React.Component{
+  render () {
+    return (<div>Index</div>)
+  }
+}
+export default Index;
