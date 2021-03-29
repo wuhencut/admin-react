85abc3d067127f9a8fe2841d8b40c42e40293dbd
stone <stone_rivers@163.com>
2021-03-29 15:45:16 +0800

    企业认证 搜索栏写了俩，先把BD搜索组件写了

--- src/pages/CompanyAuth/index.js
+++ src/pages/CompanyAuth/index.js
@@ -1,4 +1,30 @@
-const CompanyAuth = () => {
-  return <div>企业认证</div>;
-};
+import { Form, Input, Button, Select } from "antd";
+import React from "react";
+const { Option } = Select;
+class CompanyAuth extends React.Component {
+  onFinish = (val) => {
+    console(val);
+  };
+  handleChange = (val) => {
+    console.log(val);
+  };
+  render() {
+    return (
+      <div className="page">
+        <Form layout="inline" onFinish={this.onFinish}>
+          <Form.Item name="company_name">
+            <Input placeholder="企业名称"></Input>
+          </Form.Item>
+          <Form.Item name="company_verify_status">
+            <Select style={{ width: "120px" }} defaultValue="waiting" onChange="handleChange">
+              <Option value="waiting">待审核</Option>
+              <Option value="refuse">已拒绝</Option>
+              <Option value="approve">已通过</Option>
+            </Select>
+          </Form.Item>
+        </Form>
+      </div>
+    );
+  }
+}
 export default CompanyAuth;
